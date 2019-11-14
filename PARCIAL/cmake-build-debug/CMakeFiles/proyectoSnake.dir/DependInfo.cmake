4807b3d3b5b74b298e86216d7356cc8ced31161f
mmoreno750 <56569315+mmoreno750@users.noreply.github.com>
2019-11-13 23:37:57 -0300

    ENTREGA DE PARCIAL

---
+++ PARCIAL/cmake-build-debug/CMakeFiles/proyectoSnake.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Usuario/Desktop/GIT-Repositorio/INFO-2-IUA-Proyecto-Juego-UltimaversionxD/PARCIAL/comida.cpp" "C:/Users/Usuario/Desktop/GIT-Repositorio/INFO-2-IUA-Proyecto-Juego-UltimaversionxD/PARCIAL/cmake-build-debug/CMakeFiles/proyectoSnake.dir/comida.cpp.obj"
+  "C:/Users/Usuario/Desktop/GIT-Repositorio/INFO-2-IUA-Proyecto-Juego-UltimaversionxD/PARCIAL/main.cpp" "C:/Users/Usuario/Desktop/GIT-Repositorio/INFO-2-IUA-Proyecto-Juego-UltimaversionxD/PARCIAL/cmake-build-debug/CMakeFiles/proyectoSnake.dir/main.cpp.obj"
+  "C:/Users/Usuario/Desktop/GIT-Repositorio/INFO-2-IUA-Proyecto-Juego-UltimaversionxD/PARCIAL/menu.cpp" "C:/Users/Usuario/Desktop/GIT-Repositorio/INFO-2-IUA-Proyecto-Juego-UltimaversionxD/PARCIAL/cmake-build-debug/CMakeFiles/proyectoSnake.dir/menu.cpp.obj"
+  "C:/Users/Usuario/Desktop/GIT-Repositorio/INFO-2-IUA-Proyecto-Juego-UltimaversionxD/PARCIAL/score.cpp" "C:/Users/Usuario/Desktop/GIT-Repositorio/INFO-2-IUA-Proyecto-Juego-UltimaversionxD/PARCIAL/cmake-build-debug/CMakeFiles/proyectoSnake.dir/score.cpp.obj"
+  "C:/Users/Usuario/Desktop/GIT-Repositorio/INFO-2-IUA-Proyecto-Juego-UltimaversionxD/PARCIAL/textos.cpp" "C:/Users/Usuario/Desktop/GIT-Repositorio/INFO-2-IUA-Proyecto-Juego-UltimaversionxD/PARCIAL/cmake-build-debug/CMakeFiles/proyectoSnake.dir/textos.cpp.obj"
+  "C:/Users/Usuario/Desktop/GIT-Repositorio/INFO-2-IUA-Proyecto-Juego-UltimaversionxD/PARCIAL/vibora.cpp" "C:/Users/Usuario/Desktop/GIT-Repositorio/INFO-2-IUA-Proyecto-Juego-UltimaversionxD/PARCIAL/cmake-build-debug/CMakeFiles/proyectoSnake.dir/vibora.cpp.obj"
+  "C:/Users/Usuario/Desktop/GIT-Repositorio/INFO-2-IUA-Proyecto-Juego-UltimaversionxD/PARCIAL/vida.cpp" "C:/Users/Usuario/Desktop/GIT-Repositorio/INFO-2-IUA-Proyecto-Juego-UltimaversionxD/PARCIAL/cmake-build-debug/CMakeFiles/proyectoSnake.dir/vida.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Program Files (x86)/mingw-w64/i686-7.3.0-posix-dwarf-rt_v5-rev0/mingw32/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
